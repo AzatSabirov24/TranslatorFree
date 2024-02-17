19e24c1992a3898a6386758aff8ee6da73b6b86d
Azat Sabirov <azatsabirov466@gmail.com>
2024-02-17 14:34:26 +0600

    set compose deps to core_ui module

--- core_ui/build.gradle.kts
+++ core_ui/build.gradle.kts
@@ -1,3 +1,14 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.asabirov.core_ui"
+}
+
+dependencies {
+
+    // Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
 }
