554d4ee2a8f5922446d18bc5fba1a1fbf122f7d1
Azat Sabirov <azatsabirov466@gmail.com>
2024-02-21 13:49:31 +0600

    updated gradle in translation

--- feature/translation/build.gradle.kts
+++ feature/translation/build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.gradle.plugin)
 }
 
 android {
     namespace = "com.asabirov.translation"
     compileSdk = libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
 }
 
 dependencies {
@@ -21,4 +27,8 @@ dependencies {
 
     // coil
     implementation(libs.coil.compose)
+
+    // hilt
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.android.compiler)
 }
