7d11c6c204480290201e16b9dc5ffcf49b8a075b
Azat Sabirov <azatsabirov466@gmail.com>
2024-02-27 11:28:17 +0600

    fixed language drop down

--- feature/translation/src/main/kotlin/com/asabirov/translation/presentation/components/LanguageDropDownItem.kt
+++ feature/translation/src/main/kotlin/com/asabirov/translation/presentation/components/LanguageDropDownItem.kt
@@ -1,12 +1,15 @@
 package com.asabirov.translation.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -22,15 +25,20 @@ fun LanguageDropDownItem(
         onClick = onClick,
         modifier = modifier,
         text = {
-            Image(
-                painter = painterResource(id = language.drawableRes),
-                contentDescription = language.language.langName,
-                modifier = Modifier.size(40.dp)
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(
-                text = language.language.langName
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = language.drawableRes),
+                    contentDescription = language.language.langName,
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = language.language.langName
+                )
+            }
         }
     )
 }
